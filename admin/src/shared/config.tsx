7b0a3b7ffae2f1c075a2ae80d6b2d18ae78bdfb2
maroneed <gabriell.appella@gmail.com>
2023-09-21 20:32:02 -0300

    Change api url

--- admin/src/shared/config.tsx
+++ admin/src/shared/config.tsx
@@ -1,4 +1,4 @@
-export const API_URL = "http://localhost:8080/api/";
-// export const API_URL = "https://union-platform-server-api.vercel.app/api/";
+// export const API_URL = "http://localhost:8080/api/";
+export const API_URL = "https://union-platform-server-api.vercel.app/api/";
 export const DATE_FORMAT = "DD/MM/YYYY";
 export const DATE_TIME_FORMAT = "DD/MM/YYYY HH:mm:ss";
